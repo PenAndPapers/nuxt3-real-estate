34773a45ce7f952d5a3d2a4b6253a0481a30f6cc
orkestra-steven-pilagan <steven.pilagan@orkestra.energy>
2023-02-26 15:10:16 +0800

    main - update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap' }
+      ]
+    }
   },
   css: ['~/assets/css/main.css'],
   postcss: {
